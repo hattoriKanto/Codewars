5529c0a660dbdc744cf6917f587b1486b8f46708
ibag <bahniukihor@gmail.com>
2023-01-25 23:15:25 +0200

    25.01.23

--- main.js
+++ main.js
@@ -124,3 +124,28 @@ function summation(num){
 }
 
 summation(8);
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+function positiveSum(arr){
+    let sum = 0;
+    if(arr.length > 0){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > 0){
+                sum += arr[i]
+            }
+        }
+    } else{
+        sum = 0
+    }
+    console.log(sum)
+    return sum
+}
+
+positiveSum([1,2,3,4,5])
